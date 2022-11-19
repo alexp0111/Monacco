a16599f63b4ce4c07e793dcf76eb02a6c7d9de15
Alexander <alexp0111@mail.ru>
2022-11-19 19:57:24 +0300

    first screen blank [v1.0]

--- app/src/main/java/com/example/monacco/MainActivity.java
+++ app/src/main/java/com/example/monacco/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.monacco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Color of status bar
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.gray_900));
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new MainFragment()).commit();
     }
 }
