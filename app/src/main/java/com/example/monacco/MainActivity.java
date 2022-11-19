18eacc2282c90735b7b6933c0792484ae1424d30
Alexander <alexp0111@mail.ru>
2022-11-19 18:31:16 +0300

    init

---
+++ app/src/main/java/com/example/monacco/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.monacco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
